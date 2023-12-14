c770950b2ff6eb4d049b5481332832943c89eb37
Leandro <atiladevelop@gmail.com>
2023-12-14 02:39:23 -0300

    New server component added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
-export default function Home() {
-  return <div>Home Page</div>;
+import { db } from '@/db';
+
+export default async function Home() {
+  const snippets = await db.snippet.findMany();
+
+  const renderedSnippets = snippets.map((snippet) => {
+    return (
+      <div key={snippet.id}>
+        <h1 className='font-bold mb-2 mt-2'>{snippet.title}</h1>
+        <code className='font-mono text-xs p-2 border border-slate-500 bg-slate-100 rounded'>
+          {snippet.code}
+        </code>
+        <hr className='my-6' />
+      </div>
+    );
+  });
+
+  return <div>{renderedSnippets}</div>;
 }
