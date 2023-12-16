c8fb8724e123b778a18c72bec929a6ed51e53adf
Leandro <atiladevelop@gmail.com>
2023-12-16 16:23:35 -0300

    chore: working with useFormState

--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,24 +1,15 @@
-import { db } from '@/db';
-import { redirect } from 'next/navigation';
+'use client';
+
+import { useFormState } from 'react-dom';
+import * as actions from '@/actions';
 
 export default function SnippetCreatePage() {
-  async function createSnippet(formData: FormData) {
-    'use server';
-
-    const title = formData.get('title') as string;
-    const code = formData.get('code') as string;
-
-    const snippet = await db.snippet.create({
-      data: {
-        title,
-        code,
-      },
-    });
-    redirect('/');
-  }
+  const [formState, action] = useFormState(actions.createSnippet, {
+    message: '',
+  });
 
   return (
-    <form action={createSnippet}>
+    <form action={action}>
       <h3 className='font-bold m-3'>Create a Snippet</h3>
       <div className='flex flex-col gap-4'>
         <div className='flex gap-4'>
@@ -44,6 +35,7 @@ export default function SnippetCreatePage() {
             className='border rounded p-2 w-full'
           />
         </div>
+        {formState.message}
         <button type='submit' className='rounded p-2 bg-blue-200'>
           Create
         </button>
